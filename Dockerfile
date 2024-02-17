aaaa43c5cf431cecb8847f7d89147ff679f143ac
Arsalan Sefidgar <a.sefidgar@pep.co.ir>
2024-02-17 17:52:50 +0330

    init

---
+++ Dockerfile
@@ -0,0 +1,28 @@
+FROM python:3.12.1-slim-bookworm
+
+ENV PYTHONUNBUFFERED 1
+
+ENV SUPERCRONIC_URL=https://github.com/aptible/supercronic/releases/download/v0.2.29/supercronic-linux-amd64 \
+    SUPERCRONIC=supercronic-linux-amd64 \
+    SUPERCRONIC_SHA1SUM=cd48d45c4b10f3f0bfdd3a57d054cd05ac96812b \
+    TZ=Asia/Tehran
+
+WORKDIR /usr/src/app
+
+RUN apt-get update && apt-get install build-essential curl -y
+
+RUN curl -fsSLO "$SUPERCRONIC_URL" \
+ && echo "${SUPERCRONIC_SHA1SUM}  ${SUPERCRONIC}" | sha1sum -c - \
+ && chmod +x "$SUPERCRONIC" \
+ && mv "$SUPERCRONIC" "/usr/local/bin/${SUPERCRONIC}" \
+ && ln -s "/usr/local/bin/${SUPERCRONIC}" /usr/local/bin/supercronic
+
+COPY requirements.txt .
+
+RUN pip install --no-cache-dir --upgrade pip && \
+    pip install --no-cache-dir -r requirements.txt
+
+COPY ./main.py ./main.py
+COPY ./my-crontab ./my-crontab
+
+CMD [ "supercronic", "./my-crontab"]
---
+++ main.py
@@ -0,0 +1,46 @@
+import requests
+import os
+# from dotenv import load_dotenv
+
+# load_dotenv()
+
+bot_token = os.getenv('BOT_TOKEN')
+chat_id = os.getenv('CHAT_ID')
+
+print(f"debug: bot_token: {bot_token}, chat_id: {chat_id}")
+
+def send_telegram_message():
+    send_message_url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
+    # TODO: change text
+    payload = {
+        "chat_id": chat_id,
+        "text": "https://safar724.com/bus/tehran-tabriz?date=1402-12-02"
+    }
+    try:
+        requests.post(send_message_url, json=payload, timeout=2)
+    except requests.exceptions.Timeout:
+        print("Timeout error")
+
+# TODO: change url
+url = 'https://safar724.com/bus/getservices?origin=11320000&destination=26310000&date=1402-12-02'
+response = requests.get(url)
+alert = False
+
+if response.status_code == 200:
+    data = response.json()
+    for item in data['Items']:
+        departure_time = int(item['DepartureTime'].split(':')[0])
+        available_seat_count = item['AvailableSeatCount']
+
+        if (departure_time > 0) and (available_seat_count > 0):
+            print(item['DepartureTime'], item['AvailableSeatCount'])
+            alert = True
+            break
+else:
+    print(f"Failed to retrieve data: {response.status_code}")
+
+if alert:
+    print("Fire alert")
+    send_telegram_message()
+else:
+    print("Nothing to fire")
