b2a4b89f3615e2a2cabada60a37d0aaac7f51253
Arsalan Sefidgar <a.sefidgar@pep.co.ir>
2024-02-18 14:43:49 +0330

    init go

---
+++ golang/main.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Data struct {
+	Items []struct {
+		AvailableSeatCount uint8  `json:"availableSeatCount"`
+		DepartureTime      string `json:"departureTime"`
+	} `json:"items"`
+}
+
+func sendPostRequest(url string, payload map[string]interface{}) error {
+	jsonPayload, err := json.Marshal(payload)
+	if err != nil {
+		return fmt.Errorf("error marshalling JSON payload: %w", err)
+	}
+
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonPayload))
+	if err != nil {
+		return fmt.Errorf("error creating POST request: %w", err)
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return fmt.Errorf("error sending POST request: %w", err)
+	}
+	defer resp.Body.Close()
+
+	respBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return fmt.Errorf("error reading POST response body: %w", err)
+	}
+
+	fmt.Println("POST response:", string(respBody))
+	return nil
+}
+
+func main() {
+	url := os.Getenv("URL_ADDRESS")
+
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println("Error fetching JSON data:", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Error reading response body:", err)
+		return
+	}
+
+	var data Data
+	err = json.Unmarshal(body, &data)
+	if err != nil {
+		fmt.Println("Error unmarshalling JSON data:", err)
+		return
+	}
+
+	fmt.Println(data.Items)
+
+	for _, item := range data.Items {
+		parts := strings.Split(item.DepartureTime, ":")
+		if len(parts) != 2 {
+			fmt.Println("Invalid departure time format:", item.DepartureTime)
+			return
+		}
+
+		departureHour, err := strconv.Atoi(parts[0])
+		if err != nil {
+			fmt.Println("Error converting departure hour to integer:", err)
+			return
+		}
+
+		fmt.Println(departureHour)
+
+		if departureHour > 0 {
+			postURL := "https://httpbin.org/post"
+
+			payload := map[string]interface{}{
+				"availableSeatCount": item.AvailableSeatCount,
+				"departureTime":      item.DepartureTime,
+			}
+
+			err := sendPostRequest(postURL, payload)
+			if err != nil {
+				fmt.Println("Error sending POST request:", err)
+				return
+			}
+		}
+	}
+}
